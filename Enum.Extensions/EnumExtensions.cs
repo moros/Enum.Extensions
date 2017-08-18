4136ea4f48557e4fcc22bc7642d00410649cb4e1
Doug Mason <dmason@unitedfiregroup.com>
2017-08-18 10:22:20 -0500

    Adding ToDescription to make it easy to get a description of an enum or other struct value.

---
+++ Enum.Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Enum.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescription<TEnum>(this TEnum enumValue) where TEnum : struct
+        {
+            return DescriptionAttr(enumValue);
+        }
+
+        private static string DescriptionAttr<T>(this T source)
+        {
+            var field = source.GetType().GetField(source.ToString());
+            var attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes.Length > 0 ? attributes[0].Description : source.ToString();
+        }
+    }
+}
